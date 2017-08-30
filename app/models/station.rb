5861b33ea05a57c9a652e021d73c922eeb75d806
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-08-30 10:43:54 -0600

    NREL service and Station object build out

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,4 @@
-class Station 
+class Station
   attr_reader :name
               :address
               :city
@@ -8,9 +8,26 @@ class Station
               :distance
               :access_days_times
 
-  def self.station_search(zip)
-    NrelCall.alt_search(zip)
+  def initialize(params)
+    byebug
 
+    @name = params[:name]
+    @address = params[:]
+    @city = params[:]
+    @state = params[:]
+    @zip = params[:]
+    @fuel_type = params[:]
+    @distance = params[:]
+    @access_days_times = params[:]
+
+  end
+
+  def self.station_search(zip)
+    stations = NrelCall.alt_search(zip)
+    stations.each do |station|
+      Station.new(station)
+    end
+    byebug
   end
 
 end
--- app/services/nrel_call.rb
+++ app/services/nrel_call.rb
@@ -1,11 +1,21 @@
 class NrelCall
 
   def self.alt_search(zip)
-
+    response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["NREL_KEY"]}&location=#{zip}&radius=6&fuel_type=ELEC,LPG&limit=10")
+    JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
   end
 
-  # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-  # And the stations should be limited to Electric and Propane
-
-
 end
+
+
+[
+       {
+           "access_days_time": "24 hours daily",
+           "fuel_type_code": "ELEC",
+           "station_name": "UDR",
+           "city": "Denver",
+           "state": "CO",
+           "street_address": "800 Acoma St",
+           "zip": "80204",
+           "distance": 0.31422
+       },
